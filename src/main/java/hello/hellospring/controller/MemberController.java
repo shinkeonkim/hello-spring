fc9033715eafcdc77529f026a503207d2aa27b46
shinkeonkim <singun11@kookmin.ac.kr>
2022-06-25 13:35:49 +0900

    의존성 주입에 대해

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -9,6 +9,15 @@ public class MemberController {
 
     private final MemberService memberService;
 
+//    dependency injection - 필드 주입
+//    @Autowired private final MemberService memberService;
+
+//    dependency injection - setter 주입
+//    @Autowired
+//    public  void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+//    의존 관계가 실행 중에 동적으로 변하는 경우는 대부분 없으므로, 생성자 주입을 권장한다.
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
